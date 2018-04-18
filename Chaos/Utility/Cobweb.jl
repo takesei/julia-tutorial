28fb68aade19e0db0ad17280bd884e54e9b4e527
take-sei <sei06k14@gmail.com>
2018-04-19 02:10:22 +0900

    Add Feature, cobweb_plot()@lib, Refactor@cobweb

--- Chaos/Utility/Cobweb.jl
+++ Chaos/Utility/Cobweb.jl
@@ -1,21 +1,14 @@
 module Cobweb
 
-export cobweb
+export cobweb, cobweb_plot
+
+using Plots
+gr()
 
   _rendering(x, f) = [[x,x],[x,f(x)]], [[x,f(x)],[f(x),f(x)]], f(x)
   _initial_rendering(x, f) = [[x,x],[x,f(x)]], [[0,f(x)],[f(x),f(x)]], f(x)
 
   function cobweb(f, x_0, N=100)
-    """
-    cobweb(f, x_0, N=1000) -> [x, y]
-
-    When you use this with Plots,
-    Do not forget to assign "" in the label property
-
-    ex. 
-    x, y = cobweb(x_0, f)
-    plot!(x, y, label="")
-    """
     x, y, v = _initial_rendering(x_0, f)
     for i=2:N
       _x, _y, v = _rendering(v, f)
@@ -23,5 +16,24 @@ export cobweb
       append!(y, _y)
     end
     return [x, y]
+  end
+
+  function cobweb_plot(f, domein, x_0, N=100)
+    range = [f(i)for i in domein]
+    diagonal = copy(domein)
+    plot(domein, [range, diagonal], label="", lw=2)
+    x, y = [], []
+
+    for _x_0 in x_0
+      _x, _y = cobweb(f, _x_0, N)
+      append!(x, _x)
+      append!(y, _y)
+    end
+
+    if maximum(range) < maximum(diagonal)
+      plot!(x, y, label="", color="green", xlims=[minimum(domein), maximum(domein)], ylim=[minimum(diagonal), maximum(diagonal)])
+    else
+      plot!(x, y, label="", color="green", xlims=[minimum(domein), maximum(domein)], ylim=[minimum(range), maximum(range)])
+    end
   end
 end
